9674410d3b6fc54016baf8813bdc880a96d36030
shubham.khapra <shubham.khapra@scalenut.com>
2022-10-10 23:25:07 +0530

    update sevreless

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const serverless = require("serverless-http");
+const app = express()
+const port = 3000
+
+app.get('/hello', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+module.exports.handler = serverless(app);
