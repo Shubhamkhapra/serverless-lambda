da24ca40493c00b446fcdb9247f190924cc8b5ea
shubham.khapra <shubham.khapra@scalenut.com>
2022-10-11 00:05:16 +0530

    one hello

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const serverless = require("serverless-http");
 const app = express()
 const port = 3000
 
-app.get('/hello', (req, res) => res.send('Hello World! one'))
+app.get('/hello', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
