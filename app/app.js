c0e8ef9bd280f516b0bbaf247e589ae62d4e9af8
shubham.khapra <shubham.khapra@scalenut.com>
2022-10-11 09:57:34 +0530

    update port no

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const serverless = require("serverless-http");
 const app = express()
-const port = 3000
+const port = 3001
 
 app.get('/hello', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
